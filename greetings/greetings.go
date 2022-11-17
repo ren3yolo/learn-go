84a8d8843cae59860b4a1da7d70fe7465a58ddcf
Raj Shekhar <channiecx954@gmail.com>
2022-11-17 13:24:43 +0530

    Day 5 Use math/rand to generate a random greeting

--- greetings/greetings.go
+++ greetings/greetings.go
@@ -4,7 +4,24 @@ package greetings
 import (
 	"errors"
 	"fmt"
+	"math/rand"
+	"time"
 )
+
+// init sets initial values for variables used in the function.
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomFormat() string {
+	formats := []string{
+		"Hi, %v. Welcome !",
+		"%v sliding into the matrix",
+		"Hail %v, Well met",
+	}
+
+	return formats[rand.Intn(len(formats))]
+}
 
 // implement package's hello function. A function whose name starts with a capital letter can be called by a function
 // not in the same package.
@@ -14,6 +31,6 @@ func Hello(name string) (string, error) {
 		return "", errors.New("empty name")
 	}
 
-	message := fmt.Sprintf("Hi, %v. Welcome !", name)
+	message := fmt.Sprintf(randomFormat(), name)
 	return message, nil
 }
